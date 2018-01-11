ff93b76f2ecdb09e6f2a25baa7dfc35a9e19d212
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-01-11 13:06:25 -0500

    Finished constructors

--- constructors.js
+++ constructors.js
@@ -3,10 +3,35 @@
 // problem #1
 // add a method to Animal's prototype called 'grow'
 // when 'grow' is invoked log '<name> grew larger!'
+// Prototype is a SPECIAL object that is assigned objects when they are created.
+// We use the prototype to pass on abilities to child objects.
 
+//----- pseudoclassical inheritance -----
 function Animal(options) {
+  this.species = options.species;
   this.name = options.name;
 }
+
+Animal.prototype.grow = function() {
+  console.log(`${this.name} grew larger`)
+}
+
+function Dog(dogAttributes) {
+  Animal.call(this, dogAttributes);
+  this.speak = dogAttributes.speak;
+  this.waggyTail = dogAttributes.waggyTail;
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+const grizzly = new Dog({
+  species: 'Canis cuteus',
+  name: 'Grizzly',
+  speak: 'woof',
+  waggyTail: true,
+})
+
+grizzly.grow();
+console.log(grizzly)
 
 // add 'grow' to Animal's prototype here
 
@@ -16,18 +41,21 @@ function Animal(options) {
 // Cat should have its prototype inherit from Animal
 // instances of Cat should also have access to the 'grow' method
 
-function Cat(options) {
+function Cat(catAttributes) {
+  Animal.call(this, catAttributes);
   // invoke Animal here with .call
 }
 
+Cat.prototype = Object.create(Animal.prototype);
 // connect the prototypes here
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
+console.log(foofie)
 
